92ea99f01e7b3fb528065050bdfe873896f476a6
Raycent <raycent.tan@flatironschool.com>
2017-04-05 18:40:01 -0400

    Initialize

---
+++ resume.js
@@ -0,0 +1,13 @@
+{
+  "name": "Raycent Tan",
+  "address": "New York, NY",
+  "phone": "(646) 515-5434",
+  "email": "raycent.tan@flatironschool.com",
+  "shortSumary": "I am a professional Full-Stack Developer with a background in mathematical finance and strategic management.",
+  "services": [
+    "Full-Stack Development",
+    "Data Analytics",
+    "Investment Management",
+    "Strategic Planning"
+  ]
+}
